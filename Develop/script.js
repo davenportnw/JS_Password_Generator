3e8d9204b9833953bad6e0f0553eb192841065eb
davenportnw <davenportnw@gmail.com>
2020-05-05 22:04:01 -0500

    Cleaning up the code.

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,8 @@ var generateBtn = document.querySelector("#generate");
 
 const lowerCaseLetters= "abcdefghijklmnopqrstuvwxyz"
 const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
+const numbers1 = "1234567890"
+const specialCharacters = "~!@#$%^&*()_-+:"
 
 
 
@@ -12,85 +13,30 @@ const upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 function writePassword() {
 
-  //Wanted length of password
-  var howLong = prompt("How long do you want your password to be?"); 
-  console.log(howLong);
+    lengthP();
+    lCase();
+    uCase();
+    numP();
+    sCharacters();
+}
 
-  checkLength(howLong);
-  
-  if (howLong < 8 || howLong > 128){
-      alert("Lengths needs to be bewteen 8 and 128 characters.");
-      console.log(howLong);
-      return writePassword();
-    
+//* Lengths of desired password *//
+
+function lengthP(){
+
+var howLong = prompt("How long do you want your password to be?"); 
+console.log(howLong);
+
+checkLength(howLong);
+
+if (howLong < 8 || howLong > 128){
+    alert("Lengths needs to be bewteen 8 and 128 characters.");
+    console.log(howLong);
+    return writePassword();
   } 
-
-  //lower case in password
-    
-  var lowerCase = window.confirm("Do you want lowercases in your password?");
-  console.log(lowerCase);
   
-  if (lowerCase === true){
-    window.alert("We will use lowercases in your password.");
-    console.log(lowerCase);
-
-  }else{
-    window.alert("We will NOT use lowercases in your password.");
-      console.log(lowerCase);
-  }
-
-
-
-//upper case in password
-
-var upperCase = window.confirm("Do you want uppercases in your password?");
-console.log(upperCase);
-
-if (upperCase === true){
-  window.alert("We will use lowercases in your password.");
-  console.log(upperCase);
-
-}else{
-  window.alert("We will NOT use lowercases in your password.");
-    console.log(upperCase);
 }
-
-
-//numbers in password
-
-var num = window.confirm("Do you want numbers in your password?");
-console.log(num);
-
-if (num === true){
-  window.alert("We will use numbers in your password.");
-  console.log(num);
-
-}else{
-  window.alert("We will NOT use numbers in your password.");
-    console.log(num);
-}
-
-// special characters in password
-
-var sChar = window.confirm("Do you want special characters in your password?");
-console.log(sChar);
-
-if (sChar === true){
-  window.alert("We will use special characters in your password.");
-  console.log(sChar);
-
-}else{
-  window.alert("We will NOT use special characters in your password.");
-    console.log(sChar);
-}
-
-
-
-
-
-
 // Make sures the input is a number.
-
 
 function checkLength(howLong) {
   if (isNaN(howLong)){
@@ -99,14 +45,74 @@ function checkLength(howLong) {
   }
   }
 
+//* lower case in password *//
+
+function lCase(){
+  var lowerCase = window.confirm("Do you want lowercases in your password?");
+  console.log(lowerCase);
+  
+  if (lowerCase === true){
+    window.alert("We will use lowercases in your password.");
+    console.log(lowerCase);
+  }else{
+    window.alert("We will NOT use lowercases in your password.");
+      console.log(lowerCase);
+  }
+
+}
+
+
+//* upper case in password *//
+function uCase(){
+  var upperCase = window.confirm("Do you want uppercases in your password?");
+  console.log(upperCase);
+  
+  if (upperCase === true){
+  window.alert("We will use lowercases in your password.");
+  console.log(upperCase);
+  }else{
+    window.alert("We will NOT use lowercases in your password.");
+    console.log(upperCase);
+  }
+}
+
+//* numbers in password *//
+function numP(){
+  var num = window.confirm("Do you want numbers in your password?");
+  console.log(num);
+
+  if (num === true){
+    window.alert("We will use numbers in your password.");
+    console.log(num);
+
+  }else{
+    window.alert("We will NOT use numbers in your password.");
+    console.log(num);
+  }
+}
+
+
+//* special characters in password *//
+function sCharacters(){
+  var sChar = window.confirm("Do you want special characters in your password?");
+  console.log(sChar);
+
+  if (sChar === true){
+    window.alert("We will use special characters in your password.");
+    console.log(sChar);
+  }else{
+    window.alert("We will NOT use special characters in your password.");
+    console.log(sChar);
+  }
 
 
 
 
+//* Generate Password *//
 
+function generatePassword(){
 
-
-
+}
 
   // var password = generatePassword();
   // var passwordText = document.querySelector("#password");
